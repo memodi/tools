2f8227647edebc049bcec9ded67d034f70a8d97c
memodi <memodi@redhat.com>
2022-06-17 12:52:20 -0400

    support for lancher vars

--- jenkins_remote/jenkins_remote.py
+++ jenkins_remote/jenkins_remote.py
@@ -4,6 +4,8 @@ import argparse
 import requests
 import sys
 import os
+import json
+from datetime import datetime
 
 JENKINS_URL = (
     "https://mastern-jenkins-csb-openshift-qe.apps.ocp-c1.prod.psi.redhat.com/job"
@@ -54,7 +56,23 @@ def install_cluster(args):
     version = args.ocp_version
     install_type = "ipi" if args.ipi else "upi"
     provider = args.provider
-    path_to_profile = f"functionality-testing/aos-{version.replace('.', '_')}/{install_type}-on-{provider}/versioned-installer-{args.profile}"
+    path_to_profile = ""
+    name = ""
+
+    if not args.name:
+        name = os.getenv("USER") + "-" + datetime.today().strftime("%m%d%H%M")
+    else:
+        name = args.name
+
+    if args.profile:
+        path_to_profile = f"functionality-testing/aos-{version.replace('.', '_')}/{install_type}-on-{provider}/versioned-installer-{args.profile}"
+    else:
+        path_to_profile = f"functionality-testing/aos-{version.replace('.', '_')}/{install_type}-on-{provider}/versioned-installer"
+
+    launcher_vars = ""
+    if args.launcher_vars:
+        launcher_vars = get_jenkins_launcher_vars(args.launcher_vars)
+
     git_localtion = f"https://gitlab.cee.redhat.com/aosqe/flexy-templates/-/raw/master/{path_to_profile}"
 
     r = requests.get(git_localtion, verify="/etc/certs/ipa.crt")
@@ -71,13 +89,35 @@ def install_cluster(args):
         auth=(os.getenv("USER"), os.getenv("JENKINS_API_TOKEN")),
         verify="/etc/certs/ipa.crt",
         params={
-            "INSTANCE_NAME_PREFIX": args.name,
+            "INSTANCE_NAME_PREFIX": name,
             "VARIABLES_LOCATION": jenkins_path_to_profile,
+            "LAUNCHER_VARS": launcher_vars,
         },
     )
     if r.status_code == 201:
         nbuild = _get_jenkins_build_number(r.headers["location"])
         print(f"triggered job to install cluster successfully with build id: {nbuild}")
+
+
+def get_jenkins_launcher_vars(vars):
+    file = ""
+    if vars.startswith("~"):
+        vars = os.path.expanduser(vars)
+    isfile = False
+    if os.path.isfile(vars):
+        file = os.path.abspath(vars)
+        isfile = True
+    varsd = None
+    if isfile:
+        fh = open(file)
+        varsd = json.load(fh)
+        fh.close()
+    else:
+        varsd = json.loads(vars)
+    built_vars = ""
+    for k, v in varsd.items():
+        built_vars += f"{k}: {v}\n"
+    return built_vars
 
 
 def _get_jenkins_build_number(location):
@@ -94,17 +134,25 @@ def args_parser():
     subparsers.required = True
     install_parser = subparsers.add_parser("install")
     install_parser.add_argument(
-        "-n", "--name", help="specify cluster name", required=True
+        "-n", "--name", help="specify cluster name", required=False
     )
     install_parser.add_argument(
         "-p",
         "--profile",
         help="specify profile to install with, NOTE: currently profiles specified as versioned-installer-{profile} are only supported",
-        required=True,
     )
     install_parser.add_argument(
-        "-v", "--ocp-version", default="4.10", help="specify ocp version"
+        "-v", "--ocp-version", default="4.11", help="specify ocp version"
     )
+
+    install_parser.add_argument(
+        "-l",
+        "--launcher-vars",
+        type=str,
+        required=False,
+        help="Flexy launcher vars, can be supplied as JSON file or str",
+    )
+
     group = install_parser.add_mutually_exclusive_group()
     group.add_argument("--ipi", action="store_false", default=True)
     group.add_argument("--upi", action="store_true", default=False)
@@ -116,7 +164,11 @@ def args_parser():
 
     destroy_parser = subparsers.add_parser("destroy")
     destroy_parser.add_argument(
-        "--build-number", "-n", required=True, help="flexy build number to destroy"
+        "--build-number",
+        "-n",
+        type=int,
+        required=True,
+        help="flexy build number to destroy",
     )
     destroy_parser.set_defaults(func=destroy_cluster)
 
